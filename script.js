644b2b877afaecdce8a2a20256dd42e2e4434744
lacj@kea.dk <lacj@kea.dk>
2021-11-11 20:00:09 +0100

    JS html don't load bg video on mobile

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-const smallDevice = window.matchMedia("(max-width: 576px)").matches;
-if (smallDevice) {
-  document.querySelector("#videoBG").remove();
-  const img = document.createElement("img");
-  img.src = "2.png";
-  img.alt = "";
-  document.querySelector("header").appendChild(img);
+const largeDevice = window.matchMedia("(min-width: 576px)").matches;
+
+// https://stackoverflow.com/questions/28605591/prevent-html5-videos-from-downloading-the-files-on-mobile-videojs
+if (largeDevice) {
+  //video sources
+  const sources = document.querySelectorAll("video#videoBG source");
+  // reference for the video object the sources is contained inside
+  const myVideo = document.querySelector("video#videoBG");
+
+  for (var i = 0; i < sources.length; i++) {
+    sources[i].setAttribute("src", sources[i].getAttribute("data-src"));
+  }
+
+  myVideo.load();
 }
